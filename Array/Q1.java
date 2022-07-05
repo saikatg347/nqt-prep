01f5181b003224444154e0b0e7e5f1ef9dc49e3c
Saikat Das <saikat.g347@gmail.com>
2022-07-05 22:05:56 +0530

    added comments to Q.1.java

--- Array/Q1.java
+++ Array/Q1.java
@@ -4,9 +4,11 @@ public class Q1 {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
 
+    // loop for text case 👇
     int t = in.nextInt();
     while(t > 0) {
       
+      // taking user input 👇
       int n = in.nextInt();
       int[] arr = new int[n];
       
@@ -14,6 +16,7 @@ public class Q1 {
         arr[i] = in.nextInt();
       }
 
+      // function call 👇
       System.out.print( getMin(arr) );
       
       t--;
@@ -21,13 +24,17 @@ public class Q1 {
   }
 
   static int getMin(int[] arr) {
-    int min = arr[0];
-    for(int i = 0; i < arr.length; i++) {
+    int min = arr[0]; // 👈 variable to store the minimum element to far
+
+    for(int i = 0; i < arr.length; i++) { // 👈 for loop to iterate over every element of the array on by one
+
+      // if ith element is less than min then store that in min 👇`
       if(arr[i] < min) {
         min = arr[i];
       }
     }
 
+    // after for loop ends min variable should contain the minimum variable 👇
     return min;
   } 
 

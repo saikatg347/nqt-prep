1b803d5500e375a479be15e139ee140535359f90
Saikat Das <saikat.g347@gmail.com>
2022-07-06 09:34:48 +0530

    modified q3.java

--- Array/Q3.java
+++ Array/Q3.java
@@ -1,18 +1,23 @@
 public class Q3 {
 
   public static void main(String[] args) {
-    int arr[] = {5, 2, 1, 3, 4};
+    int arr[] = {8, 1 , 2};
 
     int min = arr[0];
 
-    int secMin;
+    int secMin = 999999;
 
     for(int i = 0; i < arr.length; i++) {
       if(arr[i] < min) {
+        secMin = min;
         min = arr[i];
-        secMin = 
+      }
+      else if(arr[i] < secMin) {
+        secMin = arr[i];
       }
     }
+
+    System.out.println(secMin);
   }
 
 
